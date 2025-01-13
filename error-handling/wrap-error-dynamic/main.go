997bf83803c85d604eef9bb8c2c038713333c133
ngicks <yknt.bsl@gmail.com>
2025-01-13 08:59:16 +0000

    WIP

--- error-handling/wrap-error-dynamic/main.go
+++ error-handling/wrap-error-dynamic/main.go
@@ -13,11 +13,18 @@ func main() {
 		err3 = errors.New("3")
 	)
 
+	fmt.Printf("errors.Join: %v\n", errors.Join(err1, err2, err3))
+	/*
+	   errors.Join: 1
+	   2
+	   3
+	*/
+
 	errs := []any{err1, err2, err3}
 
 	const sep = ", "
 	format, _ := strings.CutSuffix(strings.Repeat("%w"+sep, len(errs)), sep)
-	wrapped := fmt.Errorf(format, errs...)
+	wrapped := fmt.Errorf("foobar error: "+format, errs...)
 
-	fmt.Printf("err = %v\n", wrapped) // err = 1, 2, 3
+	fmt.Printf("err = %v\n", wrapped) // err = foobar error: 1, 2, 3
 }
